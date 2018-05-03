edcdaed6f66170aebe161d3d0c61e25132cb0cd1
Igor-Rabodzei <igor.rabodzei@gmail.com>
2018-05-03 13:12:26 +0300

    fix spaces

--- src/routes/getAccessToken.php
+++ src/routes/getAccessToken.php
@@ -11,7 +11,73 @@ $app->post('/api/Soundcloud/getAccessToken', function ($request, $response, $arg
         $post_data = $validateRes;
     }
 
+    //forming request to vendor API<?php
+$app->post('/api/Soundcloud/getAccessToken', function ($request, $response, $args) {
+    $settings = $this->settings;
+
+    //checking properly formed json
+    $checkRequest = $this->validation;
+    $validateRes = $checkRequest->validate($request, ['clientId', 'clientSecret', 'redirectUri', 'code']);
+    if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
+        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
+    } else {
+        $post_data = $validateRes;
+    }
+
     //forming request to vendor API
+    $query_str = $settings['api_url'].'/oauth2/token';
+    $body = array();
+    $body['client_id'] = $post_data['args']['clientId'];
+    $body['client_secret'] = $post_data['args']['clientSecret'];
+    $body['redirect_uri'] = $post_data['args']['redirectUri'];
+    $body['grant_type'] = $post_data['args']['grantType'];
+		$body['code'] = $post_data['args']['code'];
+
+    //requesting remote API
+    try {
+        $client = new GuzzleHttp\Client();
+        $resp = $client->request('POST', $query_str, [
+            'form_params' => $body
+        ]);
+
+        $responseBody = $resp->getBody()->getContents();
+        $rawBody = json_decode($resp->getBody());
+
+        $all_data[] = $rawBody;
+
+        if ($response->getStatusCode() == '200') {
+            $result['callback'] = 'success';
+            $result['contextWrites']['to'] = is_array($all_data) ? $all_data : json_decode($all_data);
+        } else {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+        }
+
+    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody();
+        $result['callback'] = 'error';
+        $result['contextWrites']['to'] = json_decode($responseBody);
+
+    } catch (GuzzleHttp\Exception\ServerException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to'] = json_decode($responseBody);
+
+    } catch (GuzzleHttp\Exception\BadResponseException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to'] = json_decode($responseBody);
+
+    }
+
+
+    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
+
+});
     $query_str = $settings['api_url'].'/oauth2/token';
     $body = array();
     $body['client_id'] = $post_data['args']['clientId'];
